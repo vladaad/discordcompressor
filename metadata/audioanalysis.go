bce6cad357e5a471d7b467e5fd4f705f6c81e82c
vladaad <59199127+vladaad@users.noreply.github.com>
2021-10-12 15:34:45 +0200

    don't rewrite the entire fucking file

--- metadata/audioanalysis.go
+++ metadata/audioanalysis.go
@@ -1,36 +1,47 @@
 package metadata
 
 import (
+	"bufio"
 	"github.com/vladaad/discordcompressor/settings"
 	"github.com/vladaad/discordcompressor/utils"
-	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
 func AnalyzeAudio(filename string) float64 {
-	UUID := utils.GenUUID()
-	sFilename := strings.Split(filename, ".")
-	extension := sFilename[len(sFilename)-1]
-	outFilename := UUID + "." + extension
 	extract := exec.Command(
 		settings.General.FFmpegExecutable,
-		"-i", filename,
+		"-y", "-i", filename,
 		"-map", "0:a:0",
-		"-c", "copy",
-		outFilename,
+		"-c:a", "copy",
+		"-f", "nut",
+		utils.NullDir(),
 	)
-	err := extract.Start()
-	if err != nil {
-		panic("Couldn't start FFmpeg")
-	}
 
-	err = extract.Wait()
+	pipe, err := extract.StderrPipe()
+	scanner := bufio.NewScanner(pipe)
 	if err != nil {
 		panic(err)
 	}
 
-	bitrate := GetStats(outFilename, true).Bitrate
-	os.Remove(outFilename)
-	return bitrate
+	err = extract.Start()
+	if err != nil {
+		panic("Couldn't start FFmpeg")
+	}
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if strings.Contains(line, "bitrate=") {
+			split := strings.Split(line, "bitrate=")
+			split2 := strings.Split(split[len(split)-1], "/")
+			replaced := strings.ReplaceAll(split2[0], "kbits", "")
+			parsed, err := strconv.ParseFloat(strings.TrimSpace(replaced), 64)
+			if err != nil {
+				return 9999
+			}
+			return parsed
+		}
+	}
+	return 9999
 }
