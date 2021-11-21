4f4e14cb14ac64682ddf6f98d76fd26c030e4f29
vladaad <59199127+vladaad@users.noreply.github.com>
2021-11-21 08:48:38 +0100

    holy shit I'm so stupid

--- metadata/bitratecalc.go
+++ metadata/bitratecalc.go
@@ -9,9 +9,9 @@ import (
 
 func CalcTotalBitrate(size float64, duration float64) (float64, bool) {
 	bitrate := size / duration
-	if bitrate > settings.Encoding.BitrateLimitMax {
-		bitrate = settings.Encoding.BitrateLimitMax
-	}
+
+	bitrate = math.Min(bitrate, settings.Encoding.BitrateLimitMax)
+
 	if bitrate < settings.Encoding.BitrateLimitMin {
 		maxLength := size / settings.Encoding.BitrateLimitMin
 		log.Println("File too long! Maximum length: " + strconv.FormatFloat(maxLength, 'f', 1, 64) + " seconds")
@@ -28,12 +28,10 @@ func CalcAudioBitrate(targetBitrate float64, encoder *settings.AudioEncoder, aud
     	mult = 0.5
 	}
 	AudioBitrate := targetBitrate * mult * float64(encoder.BitratePerc) / float64(100)
-	if AudioBitrate > encoder.MaxBitrate * mult {
-		AudioBitrate = encoder.MaxBitrate * mult
-	}
-	if AudioBitrate < encoder.MinBitrate * mult {
-		AudioBitrate = encoder.MinBitrate * mult
-	}
+
+	AudioBitrate = math.Min(AudioBitrate, encoder.MaxBitrate * mult)
+	AudioBitrate = math.Max(AudioBitrate, encoder.MinBitrate * mult)
+
 	return AudioBitrate
 }
 
