8ef758c31ff3e1a920bb15704d3734f58188f2ba
vladaad <59199127+vladaad@users.noreply.github.com>
2022-01-26 10:25:59 +0100

    switch cuda scaling to bicubic

--- encoder/video/filters.go
+++ encoder/video/filters.go
@@ -18,49 +18,49 @@ func filters(video *settings.Video, pass int) (filter string, vertRes int, FPS f
 		} else {
 			FPS = float64(video.Output.Video.Limits.FPSMax)
 		}
-		filters = append(filters, "fps=" + strconv.FormatFloat(FPS, 'f', -1, 64))
+		filters = append(filters, "fps="+strconv.FormatFloat(FPS, 'f', -1, 64))
 	}
 
 	// Deduplication
 	if settings.Advanced.DeduplicateFrames && !settings.Original {
 		maxframes := FPS - 1
 		if maxframes >= 1 {
-			filters = append(filters, "mpdecimate=max=" + strconv.FormatFloat(maxframes,'f', 0, 64))
+			filters = append(filters, "mpdecimate=max="+strconv.FormatFloat(maxframes, 'f', 0, 64))
 		}
 	}
 
 	// Subtitle burning
 	if video.Output.Subs.SubFile != "" {
-		filters = append(filters, "subtitles=si=" + strconv.Itoa(video.Input.SubtitleStream) + ":f=" + video.Output.Subs.SubFile)
+		filters = append(filters, "subtitles=si="+strconv.Itoa(video.Input.SubtitleStream)+":f="+video.Output.Subs.SubFile)
 	}
 
 	// Pixfmt conversion
 	if video.Input.Pixfmt != video.Output.Video.Encoder.Pixfmt {
-		filters = append(filters, "format=" + video.Output.Video.Encoder.Pixfmt)
+		filters = append(filters, "format="+video.Output.Video.Encoder.Pixfmt)
 	}
 
 	// Resolution
 	vertRes = video.Input.Height
-	if video.Input.Height > video.Output.Video.Limits.VResMax || float64(video.Input.Width) > float64(video.Output.Video.Limits.VResMax) / 0.5625 {
+	if video.Input.Height > video.Output.Video.Limits.VResMax || float64(video.Input.Width) > float64(video.Output.Video.Limits.VResMax)/0.5625 {
 		vertRes = video.Output.Video.Limits.VResMax
 		scaleExpr := ""
 		scaleAlgo := ""
-		if float64(video.Input.Height) / float64(video.Input.Width) < 0.5625 { // 0.5625 = 16:9
-			horizRes := int(float64(video.Output.Video.Limits.VResMax) / 1.125) * 2 // very hacky way of ensuring a multiple of 2
+		if float64(video.Input.Height)/float64(video.Input.Width) < 0.5625 { // 0.5625 = 16:9
+			horizRes := int(float64(video.Output.Video.Limits.VResMax)/1.125) * 2 // very hacky way of ensuring a multiple of 2
 			scaleExpr = strconv.Itoa(horizRes) + ":-2"
 		} else {
 			scaleExpr = "-2:" + strconv.Itoa(video.Output.Video.Limits.VResMax)
 		}
 		switch settings.Decoding.ScalingHWAccel {
 		case "cuda":
-			filters = append(filters, "hwupload_cuda,scale_cuda=" + scaleExpr + ":lanczos,hwdownload,format=" + video.Output.Video.Encoder.Pixfmt)
+			filters = append(filters, "hwupload_cuda,scale_cuda="+scaleExpr+":bicubic,hwdownload,format="+video.Output.Video.Encoder.Pixfmt)
 		default:
 			if pass == 1 {
 				scaleAlgo = "bilinear"
 			} else {
 				scaleAlgo = "spline"
 			}
-			filters = append(filters, "scale=" + scaleExpr + ":flags=" + scaleAlgo)
+			filters = append(filters, "scale="+scaleExpr+":flags="+scaleAlgo)
 		}
 	}
 
